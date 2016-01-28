a00e031404aa5761ebad7acd15ca0e8fbe61dee1
Pensarfeo <dryto25@hotmail.com>
2016-01-28 12:47:23 +0100

    add pdf and svg flyer

---
+++ public/flyer.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <title>Kiron CodeSchool</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+    <link rel="icon" type="image/png" href="/img/favicon/favicon-194x194.png" sizes="194x194">
+    <link rel="icon" type="image/png" href="/img/favicon/android-chrome-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="/img/favicon/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="/img/favicon/favicon-32x32.png" sizes="32x32">
+    <link rel="shortcut icon" href="/img/favicon/favicon.ico">
+
+    <meta name="keywords" content="Education,Integration,refugee,help,job"/>
+    <meta name="description" content="Kiron ChodeSchool is a Brussels based start-up education program, we work with companies to educate refugees in solftware development and helping them find jobs."/>
+
+    <meta property="og:type"      content="website">
+    <meta property="og:title"     content="Teaching Software-Development for Integration" />
+    <meta property="og:site_name" content="Kiron ChodeSchool" />
+    <meta property="og:url"       content="https://codeschool.kiron.university">
+    <meta property="og:image"     content="https://codeschool.kiron.university/img/heading/vent2.jpg?t=12345?" />
+    <meta property="og:description" content="Kiron ChodeSchool is a Brussels based start-up professional education program. We work with companies to facilitate the integration of refugees by training them to become sucessfull software developers.">
+  </head>
+
+
+
+  <body class="boxed-layout">
+    <div class="main-container">
+
+      <section class="pb64 pb-xs-40">
+        <div id="" class="container">
+          <div class="row">
+            <div class="col-sm-12">
+              <img alt="Pic" src="img/flyer.svg">
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </body>
+
+</html>
--- public/index.php
+++ public/index.php
@@ -22,7 +22,7 @@
     <meta property="og:site_name" content="Kiron ChodeSchool" />
     <meta property="og:url"       content="https://codeschool.kiron.university">
     <meta property="og:image"     content="https://codeschool.kiron.university/img/heading/vent2.jpg?t=12345?" />
-    <meta property="og:description" content="Kiron ChodeSchool is a Brussels based start-up education program, we work with companies to educate refugees in solftware development and helping them find jobs.">
+    <meta property="og:description" content="Kiron ChodeSchool is a Brussels based start-up professional education program. We work with companies to facilitate the integration of refugees by training them to become sucessfull software developers.">
 
     <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
     <link href="css/themify-icons.css" rel="stylesheet" type="text/css" media="all" />
