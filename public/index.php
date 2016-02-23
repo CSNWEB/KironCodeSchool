98afe4c8c6ac203dce1fa3b857be4c852cf3c0fc
Pensarfeo <dryto25@hotmail.com>
2016-02-23 11:13:02 +0100

    call new version

--- public/index.php
+++ public/index.php
@@ -28,10 +28,10 @@
 
     <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
     <link href="css/themify-icons.css"    rel="stylesheet" type="text/css" media="all" />
-    <link href="css/bootstrap.min.css"    rel="stylesheet" type="text/css" media="all" />
-    <link href="css/theme.css"            rel="stylesheet" type="text/css" media="all" />
+    <link href="css/bootstrap.min.css?version=asdf"    rel="stylesheet" type="text/css" media="all" />
+    <link href="css/theme.css?version=asdf"            rel="stylesheet" type="text/css" media="all" />
     <link href="css/share.css"            rel="stylesheet" type="text/css" media="all" />
-    <link href="css/custom.css?"          rel="stylesheet" type="text/css" media="all" />
+    <link href="css/custom.css?version=asdf"          rel="stylesheet" type="text/css" media="all" />
 
     <link href='https://fonts.googleapis.com/css?family=Lato:300,400%7CRaleway:100,400,300,500,600,700%7COpen+Sans:400,500,600' rel='stylesheet' type='text/css'>
     <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,600,700" rel="stylesheet" type="text/css">
